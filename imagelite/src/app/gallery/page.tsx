e2e41bd9f1d9678be86fb16519441ef1cb5bd733
Anderson <anderson.alanfarias@gmail.com>
2025-04-03 23:51:07 -0300

    feat: rendering the images function

--- imagelite/src/app/gallery/page.tsx
+++ imagelite/src/app/gallery/page.tsx
@@ -1,15 +1,41 @@
+'use client'
+
 import { Template, ImageCards } from '@/components'
+import { useState } from 'react'
+import { useImageService } from '@/resources/image/image.service'
+import { Image } from '@/resources/image/image.resource';
 
 export default function GalleryPage() {
+
+  const useService = useImageService();
+  const [images, setImages] = useState<Image[]>([])
+
+  async function searchImages() {
+    const result = await useService.search();
+    setImages(result);
+    console.table(result)
+  }
+
+  function renderImageCard(image: Image) {
+    return (
+      <ImageCards name={image.name}
+        src={image.url}
+        size={image.size}
+        uploadDate={image.uploadDate} />
+    )
+  }
+
+  function renderImageCards() {
+    return images.map(renderImageCard)
+  }
+
   return (
     <Template>
+      <button className='bg-gray-500' onClick={searchImages}>CLIQUE</button>
       <section className="grid grid-cols-3 gap-8">
-        <ImageCards />
-        <ImageCards />
-        <ImageCards />
-        <ImageCards />
-        <ImageCards />
-
+        {
+          renderImageCards()
+        }
       </section>
     </Template>
   )
